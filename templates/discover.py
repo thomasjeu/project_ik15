7fe5d1d00f1d625c7b8411e336897a2661c1a0f0
ii21 <iris.bakker@hotmail.com>
2020-01-21 15:09:11 +0000

    discover.py

--- templates/discover.py
+++ templates/discover.py
@@ -9,3 +9,50 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import secure_filename
 
 from helpers import apology, login_required, changepassword, changeusername, changediscription
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///admin.db")
+
+app.config["IMAGE_UPLOADS"] = "static/posts"
+app.config["PROFILE_UPLOADS"] = "static/profile"
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"]
+# app.config["MAX_IMAGE_FILESIZE"] = 0.5 * 1024 * 1024
+
+
+@app.route("twodiscover")
+@login_required
+def twodiscover():
+    user_id = session.get("user_id")
+
+    titles = db.execute("SELECT title FROM users WHERE id=:user_id", user_id=user_id)
+    title = titles[0]["title"]
+
+    discriptions = db.execute("SELECT discription FROM uploads WHERE id=:user_id", user_id=user_id)
+    discription = discriptions[0]["discription"]
+
+    posts = db.execute("SELECT path FROM uploads WHERE id=:user_id", user_id=user_id)
+    picture = db.execute("SELECT image FROM users WHERE id=:user_id", user_id=user_id)
+
+    return render_template("twodiscover.html", discription=discription, title=title)
+
+
