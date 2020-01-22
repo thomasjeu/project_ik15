901872dc2369056c9365900c6fc945804f0ef4be
jochemvaneerden <jochemvaneerden@me.com>
2020-01-22 09:20:11 +0000

    ok

--- application.py
+++ application.py
@@ -340,22 +340,6 @@ def twodiscover():
     number = request.cookies["postnumber"]
 
     titles = db.execute("SELECT * FROM uploads WHERE postnumber=:postnumber", postnumber=number)
-    # title = titles[0]["title"]
-
-    # discriptions = db.execute("SELECT discription FROM uploads WHERE postnumber=:postnumber", postnumber=number)
-    # discription = discriptions[0]["discription"]
-
-    # street_one = db.execute("SELECT street FROM uploads WHERE postnumber=:postnumber", postnumber=number)
-    # street = street_one[0]["street"]
-
-    # postal_one = db.execute("SELECT postal FROM uploads WHERE postnumber=:postnumber", postnumber=number)
-    # postal = postal_one[0]["postal"]
-
-    # city_one = db.execute("SELECT city FROM uploads WHERE postnumber=:postnumber", postnumber=number)
-    # city = city_one[0]["city"]
-
-    # number_one = db.execute("SELECT number FROM uploads WHERE postnumber=:postnumber", postnumber=number)
-    # num = number_one[0]["number"]
 
     return render_template("twodiscover.html",titles=titles)
 
