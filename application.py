55df78e8d8042caf708a659d710fd2e0694e3f33
jochemvaneerden <jochemvaneerden@me.com>
2020-01-24 11:10:40 +0000

    posts profile clickable

--- application.py
+++ application.py
@@ -204,13 +204,19 @@ def profile():
     discription = discriptions[0]["discription"]
     usernames = db.execute("SELECT username FROM users WHERE id=:user_id", user_id=user_id)
     username = usernames[0]["username"]
-    posts = db.execute("SELECT path FROM uploads WHERE id=:user_id", user_id=user_id)
+    posts = db.execute("SELECT path, postnumber FROM uploads WHERE id=:user_id", user_id=user_id)
     picture = db.execute("SELECT image FROM users WHERE id=:user_id", user_id=user_id)
     followers = db.execute("SELECT userid FROM follow WHERE followid=:followid", followid=user_id)
     following = db.execute("SELECT followid FROM follow WHERE userid=:userid", userid=user_id)
     followerslist = []
     followinglist = []
     iddict = {}
+    post_dict = {}
+    print(posts)
+    if posts:
+        for post in posts:
+            post_dict[post["postnumber"]] = post["path"]
+    print(post_dict)
     if followers:
         for follower in followers:
             f = follower["userid"]
@@ -228,7 +234,7 @@ def profile():
     # Render profile page
     print(iddict)
     print(followinglist)
-    return render_template("profile.html", discription=discription, username=username, posts=posts, picture=picture, followerslist=followerslist, followinglist=followinglist, iddict=iddict)
+    return render_template("profile.html", discription=discription, username=username, picture=picture, followerslist=followerslist, followinglist=followinglist, iddict=iddict, post_dict=post_dict)
 
 @app.route("/<int:user>")
 @login_required
